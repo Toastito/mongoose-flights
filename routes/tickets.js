6fe3275da3a0b2788043557ad9ef4db8ec517f99
Roger Davila <rogerddavila@gmail.com>
2022-04-19 21:15:07 -0700

    add delete method route

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ticketsController = require('../controllers/tickets');
 
-//GET for new ticket view
+//GET Display new ticket form
 router.get('/flights/:id/tickets/new', ticketsController.new);
 
-//POST For new ticket
+//POST Create New ticket
 router.post('/flights/:id/tickets', ticketsController.create);
+
+//DELETE Delete a Ticket
+router.delete('/tickets/:id', ticketsController.delete);
 
 module.exports = router;
