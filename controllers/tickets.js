802612f12f785b79c1d3f71350bbc9723156b264
Roger Davila <rogerddavila@gmail.com>
2022-04-20 22:20:04 -0700

    add flight empty input check to create controller

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -7,14 +7,13 @@ module.exports = {
 }
 
 function newTicket(req, res) {
-  console.log('Hello From GET new');
-  console.log(req.params.id);
   res.render('tickets/new', { flightId: req.params.id });
 }
 
 function create(req, res) {
-  console.log('Hello From Post');
-  console.log(req.params.id);
+  Object.keys(req.body).forEach((key) => {
+    if (!req.body[key]) delete req.body[key];
+  });
   req.body.flight = req.params.id;
   Ticket.create(req.body, (err) => {
     res.redirect(`/flights/${req.params.id}`);
